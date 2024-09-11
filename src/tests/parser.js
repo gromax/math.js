7c57f5817b045c0230f62ee47d3a0a7adecadd1d
Maxence Klein <maxence.klein@goupill.fr>
2024-09-11 23:01:43 +0200

    test plus complet avec arbre

--- src/tests/parser.js
+++ src/tests/parser.js
@@ -1,12 +1,30 @@
 import $ from 'jquery';
 import { Parser } from '../parser/parser';
+import { build } from '../number/rpnbuilder' ;
+import { developp } from '../number/developp';
+
+function add(texte, node) {
+    let txtNode = document.createTextNode(texte);
+    let pNode = document.createElement('p');
+    pNode.appendChild(txtNode);
+    node.appendChild(pNode);
+}
+
 
 $('#form').on('submit',
     function(e) {
         e.preventDefault();
         let saisie = document.getElementById('saisie').value;
         let p = new Parser(saisie);
-        console.log(p.rpn);
-        console.log(p.messages);
+        let rpn = document.getElementById('rpn');
+        let messages = document.getElementById('messages');
+        let arbre = document.getElementById('arbre');
+        add(p.rpn.join(' ; '), rpn);
+        for (let m of p.messages) {
+            add(m, messages);
+        }
+        let node = build(p.rpn);
+        add(String(node), arbre);
+        add(String(developp(node)), arbre);
     }
 )
