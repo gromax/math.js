e835e0304e20970ac7fd3bf319005fca6b70b39e
Maxence Klein <maxence.klein@goupill.fr>
2024-09-13 18:09:27 +0200

    gestion des opérandes dans une liste

--- src/number/add.js
+++ src/number/add.js
@@ -1,9 +1,50 @@
+import _ from "lodash";
+import { Scalar } from "./scalar";
+
+
 class Add {
     #left;
     #right;
+    #items;
     constructor(left, right) {
         this.#left = left;
         this.#right = right;
+        let items = [];
+        if (left instanceof Add) {
+            items.push(...left.items);
+        } else {
+            items.push(left);
+        }
+        if (right instanceof Add) {
+            items.push(...right.items);
+        } else {
+            items.push(right);
+        }
+        this.#items = _.sortBy(items, function(item){return item.signature()});
+    }
+
+    /**
+     * 
+     * @param {Array} operandes 
+     * @returns {Add, Scalar}
+     */
+    static fromList(operandes) {
+        if (operandes.length == 0){
+            return new Scalar(0);
+        }
+        if (operandes.length == 1) {
+            return operandes[0];
+        }
+        let n = operandes.length;
+        let node = new Add(operandes[n-2], operandes[n-1]);
+        for (let i=n-3; i>=0; i--) {
+            node = new Add(operandes[i], node);
+        }
+        return node;
+    }
+
+    get items() {
+        return [...this.#items];
     }
 
     toString() {
@@ -20,6 +61,14 @@ class Add {
 
     get right() {
         return this.#right;
+    }
+
+    /**
+    * renvoie un text donnant une représentation de l'objet sans le facteur numérique en vue de regroupement
+    * @return {string}
+    */
+    signature() {
+        return String(this);
     }
 }
 
@@ -48,6 +97,14 @@ class Minus {
 
     get right() {
         return this.#right;
+    }
+
+    /**
+    * renvoie un text donnant une représentation de l'objet sans le facteur numérique en vue de regroupement
+    * @return {string}
+    */
+    signature() {
+        return String(this);
     }
 }
 
