b59e25f49b41d3870ec0979708cd754ebd6c8887
Maxence Klein <maxence.klein@goupill.fr>
2024-09-14 18:02:19 +0200

    ajout pgcd

--- src/numbertools/misc.js
+++ src/numbertools/misc.js
@@ -28,6 +28,24 @@ function removeMinusAndDiv(node){
     return node;
 }
 
+/**
+ * Renvoie le pgcd de a, b
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function pgcd(a,b) {
+    if ((a==0) && (b==0)) {
+        throw new Error("0 % 0 n'est pas autorisé.");
+    }
+    a = Math.abs(a);
+    b = Math.abs(b);
+    while (b!=0) {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
 
-
-export { removeMinusAndDiv }
+export { removeMinusAndDiv, pgcd }
