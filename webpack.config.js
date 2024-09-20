f7b841e9e0aeff46cb557f4fc88f534d13b4ce3a
Maxence Klein <maxence.klein@goupill.fr>
2024-09-20 22:42:56 +0200

    ajout test tex

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	mode: "development",
 	entry: {
 		test_parser: './src/tests/parser.js',
+		test_tex: './src/tests/tex.js',
 		test:'./src/test.js',
 		math:'./src/maths.js'
 	},
